8d85f07e7b97233195184deba4a821ac10e65619
Josh Liddiard <joshliddiard15@gmail.com>
2023-05-13 20:35:43 -0600

    Created app documentation

--- index.js
+++ index.js
@@ -4,22 +4,28 @@ const contacts = require('./routes/contacts');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 app
-    .use(bodyParser.json())
-    .use('/', require('./routes'))
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-      })
-    .use('/contacts', contacts)
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use(cors())
+  .use(bodyParser.json())
+  .use('/', require('./routes'))
+  .use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      next();
+    })
+  .use('/contacts', contacts)
 
 mongodb.initDb((err, mongodb) => {
-    if (err) {
-      console.log(err);
-    } else {
-      app.listen(port);
-      console.log(`Connected to DB and listening on ${port}`);
-    }
-  });
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
