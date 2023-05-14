563b67130b18c0860d4aaa318214488f8d7c6d56
Josh Liddiard <joshliddiard15@gmail.com>
2023-05-13 20:54:06 -0600

    Update scheme to https

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Description',
   },
-  host: 'https://web-route-test.onrender.com',
-  schemes: ['http'],
+  host: 'web-route-test.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
