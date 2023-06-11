84053a3a5b99689062484e63b569f15107246f2f
Josh Liddiard <joshliddiard15@gmail.com>
2023-06-10 18:15:03 -0600

    Updates with Auth0

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ const user = require('./routes/user');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
 const app = express();
+const { auth, requiresAuth } = require('express-openid-connect');
+const path = require('path');
+
+
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
 app
+  .use(auth(config))
   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(cors())
   .use(bodyParser.json())
@@ -20,6 +34,10 @@ app
       next();
     })
   .use('/user', user)
+  .get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in<br><a href = "/logout">Log Out</a>' : 'Logged Out <br><a href = "/login">Login</a>');
+  })
+
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const {userValidation} = require('./validation.js');
+const { requiresAuth } = require('express-openid-connect');
 
 const userController = require('../controllers/user');
 
 const router = express.Router();
 
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUser);
+router.get('/', requiresAuth(), userController.getAllUsers);
+router.get('/:id', requiresAuth(), userController.getUser);
 router.post('/', userValidation, userController.createUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
